2643753605c4c5d054f9ea24a5231315a47d8906
Brian Lee <brian.lee.1088@gmail.com>
2016-04-16 21:45:14 +0000

    finish 5.1 challenge

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -28,18 +28,22 @@
 #=> “the usual suspects”
 
 # “The case of the disappearing last letter”.<???>
-"The case of the disappearing last letter".chomp('r')
+"The case of the disappearing last letter".chop
 # => “The case of the disappearing last lette”
 
 # “The mystery of the missing first letter”.<???>
+"The mystery of the missing first letter".reverse.chop.reverse
 # => “he mystery of the missing first letter”
 
 # “Elementary  ,    my   dear      Watson!”.<???>""
+"Elementary  ,    my   dear      Watson!".gsub(/\s+/," ")
 # => “Elementary, my dear Watson!”
 
 #“z”.<???>
+"z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
 #“How many times does the letter ‘a’ appear in this string?”.<???>
+"How many times does the letter 'a' appear in this string?".count('a')
 #=> 4
