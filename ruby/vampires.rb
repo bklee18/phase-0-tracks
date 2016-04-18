e99854ff19d4927462ef5769700fce37da6ad587
Brian Lee <brian.lee.1088@gmail.com>
2016-04-18 21:52:57 +0000

    Add birth year calculation and logic test to match age and birth year.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -30,6 +30,15 @@ until employee_counter == 0
     age = gets.chomp
     age = age.to_i
   end
+  #Misread instructions, adding age matching year born code:
+  puts "What year were you born in?"
+  birth_year=gets.chomp
+  #I did 2016 or 2017 in case their birthday already happened.
+  if age+birth_year==(2016||2017)
+    correct_age=true
+  else
+    correct_age=false
+  end
   
   puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
   garlic_bread = gets.chomp
@@ -59,13 +68,13 @@ until employee_counter == 0
     end
   end
   
-  if age < 100 && garlic_bread == 'y' && health_insurance == 'y' && name != "Drake Cula" && name != "Tu Fang" && allergy_test!=false
+  if correct_age && garlic_bread == 'y' && health_insurance == 'y' && name != "Drake Cula" && name != "Tu Fang" && allergy_test!=false
     puts "Probably not a vampire."
   elsif allergy_test == false
     puts "Probably a vampire."
-  elsif age > 99 && (garlic_bread == 'n' || health_insurance == 'n')
+  elsif correct_age==false && (garlic_bread == 'n' || health_insurance == 'n')
     puts "Probably a vampire."
-  elsif age > 99 && garlic_bread == 'n' && health_insurance == 'n'
+  elsif correct_age==false && garlic_bread == 'n' && health_insurance == 'n'
     puts "Almost certainly a vampire."
   elsif name == "Drake Cula" || name == "Tu Fang"
     puts"Definitely a vampire."
