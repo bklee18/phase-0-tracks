6373ddf670bb0ca313f594347e116ae419da3dd3
Brian Lee <brian.lee.1088@gmail.com>
2016-04-20 19:59:48 +0000

    Update with Vivien's code for release 2 (5.4 challenge)

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -12,3 +12,21 @@ p array
 
 p hash.map {|thing, color| "#{thing} is not the color #{color.next}"}
 p hash
+
+#Release 2
+array = [1, 2, 3, 4, 5, 6, 6, 7, 7, 9]
+hash = {1 => "one", 2 => "two", 3 => "three", 4 => "four"}
+
+#p array
+
+#p array.delete_if {|number| number < 5}
+#p array.keep_if {|number| number < 5}
+#p array.select { |number| number.even? }
+#p array.drop_while { |number| number > 5}
+
+#p hash
+
+#p hash.delete_if {|key, value| key < 3}
+#p hash.keep_if {|key, value| key < 3}
+#p hash.select {|key, value| key.even?}
+#keep_if also stops executing the block when condition evaluates to false
