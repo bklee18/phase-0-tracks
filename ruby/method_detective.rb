96c278067b7519428c8f2f5aa25fd827ed2b50b1
Brian Lee <brian.lee.1088@gmail.com>
2016-04-21 01:55:51 +0000

    Commit after updating file with head fork to fix some typos

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -5,79 +5,46 @@
 # committed the crime, add additional calls to prove it.
 
 # "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+# => “InVeStIgAtIoN”.swapcase
+p "InVeStIgAtIoN".swapcase
 
 # "zom".<???>
 # => “zoom”
+p "zom".insert(1, 'o')
 
 # "enhance".<???>
 # => "    enhance    "
+p "enhance".center(15)
 
 # "Stop! You’re under arrest!".<???>
 # => "STOP! YOU’RE UNDER ARREST!"
+p "Stop! You’re under arrest!".upcase
 
 # "the usual".<???>
 #=> "the usual suspects"
+p "the usual"<<" suspects"
 
 # " suspects".<???>
 # => "the usual suspects"
+p "suspects".prepend"the usual "
 
 # "The case of the disappearing last letter".<???>
 # => "The case of the disappearing last lette"
+p "The case of the disappearing last letter".chop
 
 # "The mystery of the missing first letter".<???>
 # => "he mystery of the missing first letter"
+p "The mystery of the missing first letter".slice(1..-1)
 
 # "Elementary,    my   dear        Watson!".<???>
 # => "Elementary, my dear Watson!"
+p "Elementary,    my   dear        Watson!".squeeze
 
 # "z".<???>
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+p "z".ord
 
 # "How many times does the letter 'a' appear in this string?".<???>
 # => 4
-# “iNvEsTiGaTiOn”.<???>
-“iNvEsTiGaTiOn”.swapcase
-# => “InVeStIgAtIoN”
-
-“zom”.insert(2, 'o')
-# => “zoom”
-
-# “enhance”.<???>
-"enhance".center(15)
-#=> “    enhance    ”
-
-# “Stop! You’re under arrest!”.<???>
-"Stop! You’re under arrest!".upcase
-# => “STOP! YOU’RE UNDER ARREST!”
-
-# “the usual”.<???>
-"the usual"<<" suspects"
-#=> “the usual suspects”
-
-#“ suspects”.<???>
-"suspects".prepend"the usual "
-#=> “the usual suspects”
-
-# “The case of the disappearing last letter”.<???>
-"The case of the disappearing last letter".chop
-# => “The case of the disappearing last lette”
-
-# “The mystery of the missing first letter”.<???>
-"The mystery of the missing first letter".reverse.chop.reverse
-# => “he mystery of the missing first letter”
-
-# “Elementary  ,    my   dear      Watson!”.<???>"" 
-
-"Elementary  ,    my   dear      Watson!".gsub(/\s+,/, ",").gsub(/\s+/," ")
-# => “Elementary, my dear Watson!”
-
-#“z”.<???>
-"z".ord
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
-
-#“How many times does the letter ‘a’ appear in this string?”.<???>
-"How many times does the letter 'a' appear in this string?".count('a')
-#=> 4
+p "How many times does the letter 'a' appear in this string?".count('a')
