9fc42cbba02084e7785429e88fc9e9d55cb71805
Brian Lee <brian.lee.1088@gmail.com>
2016-04-26 18:00:28 +0000

    Add psuedocode for 6.1 challenge

---
+++ ruby/puppy.rb
@@ -0,0 +1,12 @@
+#Define a class named Puppy
+# =>Add methods for this Puppy class such as bark method, wagging tail method and lick face method.
+# =>Each of the above methods would output a PUTS line of the corresponding action.
+#END of class methods
+
+#Puppy.methods would return the available methods to call upon an instance of Puppy class
+# =>duchess=Puppy.new
+# =>fido=Puppy.new
+# =>spot=Puppy.new
+#The above code should produce 3 instances of the Puppy class stored in each of the variable names (duchess, fido, spot)
+#I can now call my Puppy class methods onto each of those variables
+# =>ex. duchess.bark should call my bark method onto the duchess variable.
