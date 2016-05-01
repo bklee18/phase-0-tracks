2a55dbec35f8bf8208c803c1ba2fb65d1c267f1d
Brian Lee <brian.lee.1088@gmail.com>
2016-05-01 00:58:40 +0000

    Update pseudocode and improve interface output

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,14 +1,10 @@
-#Solo Challenge - Design and Implement a Class
-#3 attributes using at least 2 data types
-#3 methods one of with takes an argument
-
 #Create Blackjack class (Aces will be worth 1)
-# =>Each initialization will start with attribute card1 and card 2 and read only attribuete of current_total
+# =>Each initialization will start with read-only attribute of current_total and will set attributes facial expression and drink which will have read/write access.
 # =>Read/write attr will include facial_expression, drink
 # =>Methods will include hit, stay, facial_expression, drink
 
 class Blackjack
-  attr_reader :card1, :card2, :current_total
+  attr_reader :current_total
   attr_accessor :facial_expression, :drink
   
   def initialize
@@ -25,6 +21,7 @@ class Blackjack
     next_card = rand(9)+1
     puts "Next card: #{next_card}"
     @current_total += next_card
+    puts "Your card total: #{current_total}"
   end
   
   def stay
@@ -47,7 +44,7 @@ class Blackjack
       puts "YOU GOT BLACKJACK!!!"
       @facial_expression="^_^"
     end
-    puts "#{@facial_expression}"
+    puts "Facial expression: #{@facial_expression}"
   end
     
   def change_facial_expression(face)
@@ -61,10 +58,12 @@ class Blackjack
   end
 end
 
+
 def interface
   puts "\n Brian's BLACKJACK Simulator 1.0 \n"
   hand=Blackjack.new
-  catch :done do
+  choice=String.new
+  while true
     puts "\n Options: face, drink, hit, stay, quit"
     choice=gets.chomp
     if choice=='face'
@@ -80,16 +79,15 @@ def interface
       hand.hit
     elsif choice=='stay'
       hand.stay
-      puts "\n Another game? (y/n)"
-      answer=gets.chomp
-      if answer=='y'
-        interface
-      elsif answer=='n'
-        throw :done
-      end
-    elsif choice=='quit'
-      throw :done
+      break
+    elsif choice=="quit"
+      break
     end
+  end
+  puts "Play another round? (y/n)"
+  answer=gets.chomp
+  if answer=='y'
+    interface
   end
 end
 
