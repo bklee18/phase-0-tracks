15edad4b95fbfceab8fd37d61afd90f7068c3854
Brian Lee <brian.lee.1088@gmail.com>
2016-05-04 09:15:42 +0000

    Finish release 1 for 7.2

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,5 +1,22 @@
+// Release 0
 var colors = ["Blue", "Green", "Pink", "Red"];
 var names = ["Ed", "Benny", "Skrillex", "Touche"];
 
 colors.push("White");
-names.push("Mr. Black");
+names.push("Mr Black");
+
+// Release 1
+// Lets make a single entry object hash first
+// var name = names[0];
+// var color = colors[0];
+// var hash = {};
+// hash[name] = color;
+// console.log(hash);
+
+// Now let's loop
+var hash = {};
+for ( var i = 0; i < names.length; i++ ) {
+  hash[names[i]] = colors[i];
+}
+
+console.log(hash);
