88e2f5c6fe21065ae2ba530bae1bb916ded68b59
Brian Lee <brian.lee.1088@gmail.com>
2016-05-12 19:48:32 +0000

    INitial commit for budget project for 8.5

---
+++ 8.5_challenge/budget.rb
@@ -0,0 +1,13 @@
+=begin
+Monthly Financial Budget Program:
+
+* Establish username in DB
+
+* Ask for positive income streams - Allow for multiple streams, as many as the user wants. Number will have to be changed to per month rate
+
+* Ask for monthly costs - Allow for multiple costs, as many as the user wants. Number will have to be changed to per monthly rate
+
+* Store all plusses and minuses for one month in a table that will use a foreign key to reference the month. 
+
+=end
+
