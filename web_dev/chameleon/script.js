657722f0c9daf6dcbaa6dbb61c15196378c60ef8
Brian Lee <brian.lee.1088@gmail.com>
2016-05-17 17:21:05 +0000

    Finish release 1 for 9.2 - created new html elements and looped through nodes in our script.js

--- web_dev/chameleon/script.js
+++ web_dev/chameleon/script.js
@@ -1,10 +1,41 @@
 console.log("The script is running!");
 
-function addPinkBorder(event) {
-  console.log("click happened! here's the click event:");
-  console.log(event);
-  event.target.style.border = "2px solid pink";
-}
+// function addPinkBorder(event) {
+//   console.log("click happened! here's the click event:");
+//   console.log(event);
+//   event.target.style.border = "2px solid pink";
+// }
 
-var photo = document.getElementById("lizard-photo");
-photo.addEventListener("click", addPinkBorder);
+// var photo = document.getElementById("lizard-photo");
+// photo.addEventListener("click", addPinkBorder);
+
+// var photo = document.getElementById("lizard-photo");
+// var hideButton = document.getElementById("hidebutton");
+
+// console.log(hideButton);
+
+// function hidePhoto(event) {
+//   console.log("hide happened");
+//   console.log(event);
+//   event.target.style.visibility = "hidden";
+// }
+
+// photo.addEventListener("click", hidePhoto);
+// hideButton.addEventListener("click", hidePhoto);
+
+// Add new element
+var newHeading = document.createElement('h1');
+var h1Text = document.createTextNode("new text node!");
+newHeading.appendChild(h1Text);
+
+var button = document.getElementById("hidebutton");
+var parent = button.parentNode;
+parent.insertBefore(newHeading, button);
+
+
+// For loop
+var listItems = document.getElementsByClassName('list');
+
+for (var i=0; i<listItems.length; i++) {
+  listItems[i].style.border = "2px solid blue";
+}
